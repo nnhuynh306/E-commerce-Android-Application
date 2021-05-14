2bce8d07247e1c59d85cf31e74184e1d6fea99b9
maidangkhanh <mai.d.khanh@gmail.com>
2021-05-14 10:45:33 +0700

    Add adapter

---
+++ src/app/src/main/java/com/example/ecommerceapp/ViewModel/ProductListViewModel.java
@@ -0,0 +1,15 @@
+package com.example.ecommerceapp.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.RealmObjects.Product;
+
+import java.util.Objects;
+
+import io.realm.Realm;
+
+public class ProductListViewModel extends ViewModel {
+
+
+}
